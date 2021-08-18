546ba710bbc94916e7bebb911fb984e8b8dc7053
Nirupa <nirupasinha4@gmail.com>
2021-08-18 20:42:39 +0530

    make folder

--- src/index.js
+++ src/index.js
@@ -1,17 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+// let city = {
+// name: "Mumbai",
+// country: "india"
+// };
+function Hello() {
+  //  return <h1>welcome tO React </h1>
+   return ( 
+   <div>
+        <h1>welcome to react</h1>
+        <h1>let's build something cool</h1>
+    </div>);
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+//React.createElement("h1", {style: { color:"red" } } , "Hello Sarvottam"),
+//<h1 class="heading">Sarvottam is living in {city.name} {city.country}</h1>,
+/* <ul>
+    <li>Rishi Prakash</li>
+    <li>Nirupa Sinha</li>
+    <li>Ved Praksh </li>
+    <li>Ravindra Prasad</li>
+    <li>Anita Devi</li>
+</ul>, */
+<Hello></Hello>,
+    document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
